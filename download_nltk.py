02c0001ebd35d78066a5d6291dc3d8fa054ec5d2
ORADEV <omotosoabiola9525@gmail.com>
2024-06-21 23:56:30 +0100

    nltk download

--- download_nltk.py
+++ download_nltk.py
@@ -1,2 +1,6 @@
 import nltk
-nltk.download('punkt')
+import os
+
+nltk_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'nltk_data')
+nltk.data.path.append(nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
